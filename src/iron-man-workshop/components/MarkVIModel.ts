1fb9b0ce6726848b4fc95ba5eb05364ed3f4a614
Logan Nguyen <logan.nguyen@swirldslabs.com>
2026-01-16 19:25:42 -0600

    chore(iron-man): renamed GLBs
    Signed-off-by: Quiet Node <logan@quiet-node.dev>

--- src/iron-man-workshop/components/MarkVIModel.ts
+++ src/iron-man-workshop/components/MarkVIModel.ts
@@ -25,7 +25,7 @@ THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
 // Import articulated GLB model with properly separated limbs
-import modelUrl from '../assets/mark-vi-articulated.glb?url';
+import modelUrl from '../assets/mark-vi-schematic.glb?url';
 
 export interface MarkVIModelConfig {
   /** Primary hologram color */
